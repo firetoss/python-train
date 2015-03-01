c68a3eebae6acd93f30128449113132ac748cc29
houyuanzhuo <hyz.firetoss@gmail.com>
2015-03-01 23:12:29 +0800

    modified

---
+++ base.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+a = 100
+if a > 0:
+    print a
+else:
+    print -a
+
+print 10 / 3
+print 10.0 / 3
+print 10 % 3
+print '中文'
+
+print 'hello %s' % 'hyz'
+print '%s\'s age is %d' % ('hyz', 31)
